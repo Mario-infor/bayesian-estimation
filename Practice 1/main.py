44429a2ff3ab6c280bd03904cec595e1cd619d20
Mario Herrera <mherreraalmira@gmail.com>
2023-08-29 17:09:33 -0600

    create mean and standard deviation for full data sets

--- Practice 1/main.py
+++ Practice 1/main.py
@@ -1,7 +1,12 @@
 import random
 
-
 if __name__ == '__main__':
-
     # Generate 500 values between 1 and 100
-    data = [random.randint(1, 100) for _ in range(500)]
+    data = [random.randint(1, 100) for _ in range(10)]
+
+    n = len(data)
+
+    mean = sum(data) / n
+
+    temp = [(x - mean) ** 2 for x in data]
+    stand_des = sum(temp) / n
