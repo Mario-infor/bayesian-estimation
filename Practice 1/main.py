1c72b05c8f43d6b9b29d8170ff0e00b467ebffd6
Mario Herrera <mherreraalmira@gmail.com>
2023-08-29 18:00:16 -0600

    add graph to compare mean when the full data set is known and whe the data set is dynamic

--- Practice 1/main.py
+++ Practice 1/main.py
@@ -1,11 +1,14 @@
 import random
 import math
+import matplotlib.pyplot as plt
+import numpy as np
 
 if __name__ == '__main__':
-    # Generate 500 values between 1 and 100
-    data = [random.randint(1, 100) for _ in range(500)]
 
-    n = len(data)
+    n = 500
+
+    # Generate 500 values between 1 and 100
+    data = [random.randint(1, 100) for _ in range(n)]
 
     full_mean = sum(data) / n
 
@@ -37,3 +40,9 @@ if __name__ == '__main__':
     print(full_stand_des)
     print(mean_trajectory[len(mean_trajectory) - 1])
     print(stand_des_trajectory[len(stand_des_trajectory) - 1])
+
+    x_mean_full = np.linspace(0, n, n)
+    y_mean_full = np.full_like(x_mean_full, full_mean)
+
+    plt.plot(x_mean_full, y_mean_full, 'r', x_mean_full[:-1], mean_trajectory, 'b')
+    pass
